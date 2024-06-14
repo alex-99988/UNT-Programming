cf2c51db39bc3715d2810b5430bb09e5c7bbcebf
equanex <alexgkostov@gmail.com>
2024-06-14 17:17:14 +0300

    Update addition.js

--- addition.js
+++ addition.js
@@ -1,1 +1,4 @@
-
+var question1 = document.getElementsByTagName('question1');
+            document.getElementById("myText").innerHTML = question1;
+            //for (var i = 0; i < elements.length; i++) {
+                //console.log(elements[i].tagName);

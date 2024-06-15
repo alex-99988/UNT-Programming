9eb2964e9bd5a313ab94cac1fd0c8a382374082d
equanex <alexgkostov@gmail.com>
2024-06-15 11:04:58 +0300

    Update division.js

--- division.js
+++ division.js
@@ -15,11 +15,11 @@ document.getElementById('userForm').addEventListener('submit', function(event) {
 
     var score = 0; // Initialize score variable
 
-    if (parseInt(question1) === 2) score++;
-    if (parseInt(question2) === 4) score++;
-    if (parseInt(question3) === 3) score++;
+    if (parseInt(question1) === 1) score++;
+    if (parseInt(question2) === 2) score++;
+    if (parseInt(question3) === 2) score++;
     if (question4 === '20') score++; // Compare the value of question4
-    if (parseInt(question5) === 18) score++; // Corrected the answer to match 14 + 4
+    if (parseInt(question5) === 2) score++; // Corrected the answer to match 14 + 4
 
     alert('Form submitted with the following data:\n' +
           'Question 1: ' + question1 + '\n' +
